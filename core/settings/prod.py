ad6dcb13b3326bd4b1c3e70bdf423aa88232dc77
fj-fj-fj <berendeyzhiv@gmail.com>
2021-03-19 16:32:25 +0300

    init

---
+++ core/settings/prod.py
@@ -0,0 +1,21 @@
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = False
+
+ADMIN = os.environ.get('ADMIN')
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+    },
+}
