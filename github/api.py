400ac02ac627b74fdd72dedfecd72a65e2fc4ef9
fj-fj-fj <berendeyzhiv@gmail.com>
2021-03-28 00:11:47 +0300

    naming: improve. typing: add

--- github/api.py
+++ github/api.py
@@ -1,19 +1,20 @@
 import requests
-
 from typing import List, Dict, Any, Optional
+
 from core.settings.base import API_URL
+from django.http import HttpRequest
 
 try:
     from core.settings.local import GITHUB_TOKEN as __T
 
-    URL = API_URL + f'&authorization_request={__T}'
+    URL: str = API_URL + f'&authorization_request={__T}'
 except ImportError:
     pass
 
 
-def get_project_user_list_with_pull_requests(requests) -> dict:
-    username: str = requests.POST.get('search').strip()
-    projects: dict = _get_pull_request_data(username)
+def show_project_list_with_pull_requests(request: HttpRequest) -> Dict[str, Any]:
+    username: str = request.POST.get('search').strip()
+    projects: dict = _generate_project_list_with_pull_requests(username)
     projects: Optional[list] = projects.get('projects')
 
     return {
@@ -22,24 +23,24 @@ def get_project_user_list_with_pull_requests(requests) -> dict:
     }
 
 
-def _get_pull_request_data(user: str) -> Dict[str, Any]:
+def _generate_project_list_with_pull_requests(user: str) -> Dict[str, Any]:
     user_url: str = URL.format(user)
-    pull_requests: list = _get_pull_request_user_list(user_url)
+    pull_requests: list = _generate_pull_request_list(user_url)
     if pull_requests:
-        projects: list = _create_and_return_projects_user_list(pull_requests)
+        projects: list = _generate_project_list(pull_requests)
         return {'projects': projects}
     return {'projects': None}
 
 
-def _get_pull_request_user_list(user: str) -> Optional[List[dict]]:
-    pull_requests: list = _get_data_from_github_API(user).get('items')
+def _generate_pull_request_list(user: str) -> Optional[List[dict]]:
+    pull_requests: list = _fetch_data_from_github_API(user).get('items')
 
     if pull_requests is None:
         return []
 
     for pull in pull_requests:
         pull_request_url: str = pull.get('pull_request').get('url')
-        pull_request_info: dict = _get_data_from_github_API(pull_request_url)
+        pull_request_info: dict = _fetch_data_from_github_API(pull_request_url)
 
         pull['merged'] = pull_request_info.get('merged')
         pull['html_url'] = pull_request_info.get('html_url')
@@ -47,13 +48,13 @@ def _get_pull_request_user_list(user: str) -> Optional[List[dict]]:
     return pull_requests
 
 
-def _create_and_return_projects_user_list(pulls: list) -> List[dict]:
-    projects = []
+def _generate_project_list(pulls: list) -> List[dict]:
+    projects: list = []
     names = set()
 
     for pull in pulls:
         repo_url: str = pull.get('repository_url')
-        repo: dict = _get_data_from_github_API(repo_url)
+        repo: dict = _fetch_data_from_github_API(repo_url)
 
         if (name := repo.get('name')) not in names:
             names.add(name)
@@ -70,5 +71,5 @@ def _create_and_return_projects_user_list(pulls: list) -> List[dict]:
     return projects
 
 
-def _get_data_from_github_API(url: str) -> Dict[str, Any]:
+def _fetch_data_from_github_API(url: str) -> Dict[str, Any]:
     return requests.get(url).json()
--- github/views.py
+++ github/views.py
@@ -1,21 +1,24 @@
+from typing import Dict, Any
+
+from django.http import HttpRequest, HttpResponse
 from django.views.generic.base import TemplateView
 from django.views import View
 from django.shortcuts import render
 
-from .api import get_project_user_list_with_pull_requests
+from .api import show_project_list_with_pull_requests
 
 
 class IndexPageView(TemplateView):
 
-    template_name = 'github/index.html'
+    template_name: str = 'github/index.html'
 
 
 class PullRequestDetailView(View):
 
-    def post(self, requests):
-        projects = get_project_user_list_with_pull_requests(requests)
-        context = {
+    def post(self, request: HttpRequest) -> HttpResponse:
+        projects: Dict[str, Any] = show_project_list_with_pull_requests(request)
+        context: Dict[str, Any] = {
             'user': projects['owner'],
             'projects': projects['projects'],
         }
-        return render(requests, 'github/project_list.html', context)
+        return render(request, 'github/project_list.html', context)
